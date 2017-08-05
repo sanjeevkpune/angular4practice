1c748dffe32f3fc82c0e22764a684f9ce98f615e
Sanjeev kumar <sanjeev.ku.new@gmail.com>
2017-08-05 13:15:09 +0530

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'; // we need to import this module in order to recognize the ngModel directive
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule  // we need to import this module in order to recognize the ngModel directive
   ],
   providers: [],
   bootstrap: [AppComponent]
